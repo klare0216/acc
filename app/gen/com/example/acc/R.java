6d283d8904f3c1035bd1858a79b1413c403ae427
klare0216 <klare0216@gmail.com>
2017-03-28 12:19:19 +0800

    update

--- app/gen/com/example/acc/R.java
+++ app/gen/com/example/acc/R.java
@@ -10,6 +10,15 @@ package com.example.acc;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int b_blue=0x7f040005;
+        public static final int blue=0x7f040003;
+        public static final int button_color=0x7f040006;
+        public static final int green=0x7f040000;
+        public static final int pink=0x7f040004;
+        public static final int red=0x7f040001;
+        public static final int white=0x7f040002;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,34 +27,36 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_bg=0x7f020000;
+        public static final int button_bg_save=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int button1=0x7f080006;
-        public static final int button2=0x7f080008;
-        public static final int editText1=0x7f080003;
-        public static final int editText2=0x7f080002;
-        public static final int textView1=0x7f080000;
-        public static final int textView2=0x7f080001;
-        public static final int textView3=0x7f080004;
-        public static final int textView4=0x7f080005;
-        public static final int textView5=0x7f080007;
+        public static final int action_settings=0x7f090009;
+        public static final int button1=0x7f090006;
+        public static final int button2=0x7f090008;
+        public static final int editText1=0x7f090003;
+        public static final int editText2=0x7f090002;
+        public static final int textView1=0x7f090000;
+        public static final int textView2=0x7f090001;
+        public static final int textView3=0x7f090004;
+        public static final int textView4=0x7f090005;
+        public static final int textView5=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
     }
     public static final class style {
         /** 
@@ -69,10 +80,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- app/src/com/example/acc/MainActivity.java
+++ app/src/com/example/acc/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.acc;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.icu.text.DecimalFormat;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,6 +24,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+@TargetApi(24)
 public class MainActivity extends Activity {
 	private SensorManager sensorMgr;
 	private TextView text;
@@ -30,10 +36,15 @@ public class MainActivity extends Activity {
 	private EditText edittext_2;
 	private float distance;
 	private float angle;
-	private float maxV;
+	private boolean start;
+	private int time;
+	private double a_max;
 	private float dsec;
 	private List <String> output_dat = new ArrayList<String>();
+	private List <String> output_time = new ArrayList<String>();
+	
 	private float error;
+	
 
 	
 	@Override
@@ -50,8 +61,11 @@ public class MainActivity extends Activity {
 		button = (Button)findViewById(R.id.button1);
 		button_2 = (Button)findViewById(R.id.button2);
 		log.setText(" ");
-		
-		button_setup();
+		edittext.setText("0");
+		edittext_2.setText("0");
+		start = false;
+		time = 0;
+		setup();
 
 		
 	}
@@ -77,21 +91,31 @@ public class MainActivity extends Activity {
 	}
 	
 	// 設定button事件
-	private void button_setup(){
+	private void setup(){
 		button.setOnClickListener(new Button.OnClickListener(){
 			 @Override 
 	         public void onClick(View v) {
 	             distance = Float.valueOf(edittext.getText().toString());
 	             angle = Float.valueOf(edittext_2.getText().toString());
-	             log.setText("dis: "+ distance+"\nangle:"+angle);
+	             a_max = 9.8*Math.sin(angle/180*Math.PI);
+	             log.setText("坡長 : "+ distance+"\n角度: "+angle+"\n"+"a_max = "+a_max+"\n");
 			 }	            
 		});
 		button_2.setOnClickListener(new Button.OnClickListener(){
 			 @Override 
 	         public void onClick(View v) {
-	             log.setText("dis: "+ distance+"\nangle:"+angle);
-	             save();
-	             log.setText("save output.txt.");
+	             if(start){
+	            	 start = false;
+	            	 time = 0;
+	            	 button_2.setText("START");
+	            	 button_2.setBackgroundResource(R.drawable.button_bg);
+	            	 //generate_data();
+	            	 save();
+	             }else {
+	            	 start = true;
+	            	 button_2.setText("SAVE");
+	            	 button_2.setBackgroundResource(R.drawable.button_bg_save);
+	             }
 			 }
 	            
 		});
@@ -99,35 +123,109 @@ public class MainActivity extends Activity {
 	
 	private void save(){
 		write();
-		maxV = 0;
 		output_dat.clear();;
 	}
 	
+
+	
+	private double averange_data(){
+		int len = output_dat.size();
+		double sum = 0;
+		DecimalFormat df=new DecimalFormat("#.###");
+		for(int i=0;i<output_dat.size();i++){
+			sum = sum + Double.parseDouble(output_dat.get(i));
+		}
+		return Double.parseDouble(df.format(sum/len));
+	}
+	
+	private double median(){
+		List <Double> tmp = new ArrayList<Double>();
+		DecimalFormat df=new DecimalFormat("#.###");
+		for(int i=25;i<output_dat.size()-26;i++){
+			tmp.add(Double.parseDouble(output_dat.get(i)));
+		}
+		Collections.sort(tmp);
+		return  Double.parseDouble(df.format(tmp.get((int)(tmp.size()/2))));
+		//return tmp.size()/2;
+	}
+	
+	private void generate_data(){
+		//a_max = 9.8 * sin(angle)
+		//double a_max = 9.8*Math.sin(angle/180*Math.PI);
+		//double factor = 1;
+		/*for(int i=0;i<output_dat.size();i++){
+			if(Double.parseDouble(output_dat.get(i))>a_max){
+				output_dat.remove(i);
+				output_time.remove(i);
+				i--;
+			}
+		}*/
+		double diff = 10000;
+		double factor = 1;
+		for(int i=1;i<output_dat.size();i++){
+			double tmp_diff ;
+			int j=1;
+			log.append(output_time.get(i-j).equals("*")+"\n");
+			while((i-j-1)>=0 && output_time.get(i-j).equals("*")){
+				j++;
+				
+			}
+			
+			tmp_diff = Double.parseDouble(output_dat.get(i)) - Double.parseDouble(output_dat.get(i-j));
+			//log.append("("+i+","+j+") = "+output_dat.get(i)+", "+output_dat.get(i-j)+"\n");
+			log.append("("+i+","+j+") : diff= "+tmp_diff+"\n");
+			if(Math.abs(diff*factor) < Math.abs(tmp_diff)){
+				// 去掉這個值
+				//output_dat.remove(i);
+				//output_time.remove(i);
+				//i--;
+				//log.append("*");
+				output_time.set(i-j,"*");
+			}else{
+				diff = tmp_diff;
+			}
+		}
+		
+	}
 	// 拿來寫入檔案
 	private void write(){
 		try{
-			// 建立FileOutputStream物件，路徑為SD卡中的output.txt
-			 FileOutputStream output = new FileOutputStream("/sdcard/output.txt");
+			 String f_name = "/sdcard/output";
+			 int count = 0;
+	    	 File file = new File(f_name+"_"+count+".txt");
+		     // find out if the name exists or not.
+	    	 while(file.exists()){
+		    	 count++;
+		    	 file=new File(f_name+"_"+count+".txt");
+		     }
+		     // write data to file
+			 FileOutputStream output = new FileOutputStream(f_name+"_"+count+".txt");
+			 
+			 log.append("平均: "+averange_data()+"\n");
+			 String tmp ="中位數: "+median()+"\n";
+			 output.write(tmp.getBytes());
+			 
 			 while(output_dat.size() != 0){
+				output.write(output_time.get(0).getBytes());
+				output.write(",".getBytes());
 				output.write(output_dat.get(0).getBytes());
 				output.write("\n".getBytes());
 			 	output_dat.remove(0);
+			 	output_time.remove(0);
 			 }
 			 output.close();
+			 log.append(f_name+"_"+count+".txt\n");
+			 //log.setText(f_name+"_"+count+".txt");
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
 	
 	private double result_a(float x,float y,float z){
-		return Math.sqrt((x*x+y*y+z*z));
+		return Math.sqrt((x*x+y*y));
 	}
 	
-	private boolean isMove(double a){
-		if(a < 9.7)
-			return true;
-		return false;
-	}
+
 	
 	SensorEventListener listener = new SensorEventListener(){
 		@SuppressLint("NewApi")
@@ -136,13 +234,17 @@ public class MainActivity extends Activity {
 			StringBuilder sensorInfo = new StringBuilder();
 			DecimalFormat df=new DecimalFormat("#.###");
 			float[] values = event.values;
-			for(int i = 0;i < values.length;i++)
-				sensorInfo.append("加速度["+i+"] = "+df.format(values[i])+"\n");
+			sensorInfo.append("加速度 x = "+df.format(values[0])+"\n");
+			sensorInfo.append("加速度 y = "+df.format(values[1])+"\n");
+			sensorInfo.append("加速度 z = "+df.format(values[2])+"\n");
 			double a = result_a(values[0],values[1],values[2]);
-			sensorInfo.append("加速度和:"+df.format(a)+"\n");
-			if (isMove(a)) sensorInfo.append("移動中");
-			else sensorInfo.append("靜止");
-			output_dat.add(a+"");
+			sensorInfo.append("加速度和 = "+df.format(a)+"\n");
+			if(start) {
+				output_dat.add(a+"");
+				output_time.add(time+"");
+				time++;
+			}
+			
 			text.setText(sensorInfo);
 			
 		}
@@ -155,15 +257,16 @@ public class MainActivity extends Activity {
 	};
 	
 	SensorEventListener listener_G = new SensorEventListener(){
+		@SuppressLint("NewApi")
 		@Override
 		public void onSensorChanged(SensorEvent event){
 			//Sensor sensor = event.sensor;
 			StringBuilder sensorInfo = new StringBuilder();
-			
+			DecimalFormat df=new DecimalFormat("#.###");
 			float[] values = event.values;
-			for(int i = 0;i < values.length;i++)
-				sensorInfo.append("-G_values["+i+"] = "+values[i]+"\n");
-
+			sensorInfo.append("陀螺儀 x = "+df.format(values[0])+"\n");
+			sensorInfo.append("陀螺儀 y = "+df.format(values[1])+"\n");
+			sensorInfo.append("陀螺儀 z = "+df.format(values[2])+"\n");
 			text_2.setText(sensorInfo);
 		}
 
